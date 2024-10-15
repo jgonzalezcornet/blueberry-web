e769a00fb59c271b53b0192395b2ad74fd4b24a4
JuaniGit <juani@Andy-PC.>
2024-10-15 21:51:29 +0000

    feat: add recover-page and some extras

--- blueberry-Web/src/pages/(login)/olvide.vue
+++ blueberry-Web/src/pages/(login)/olvide.vue
@@ -1,11 +1,75 @@
 <template>
-    <div class="header-container">
-      <h1 class="page-title">Olvide mi contraseña</h1>
+    <div class="recover-container">
+      <v-card elevation="2" class="font-montserrat px-6 pt-3 pb-5 rounded-lg recover-card">
+        <h1 class="page-title" style="margin-bottom: 20px">Recuperar contraseña</h1>
+        <p class="description">
+          Ingrese su correo electrónico para recibir un link a un formulario para recuperar su contraseña.
+        </p>
+        <v-text-field
+          label="Correo electrónico"
+          v-model="email"
+          prepend-icon="mdi-email"
+        />
+        <div class="button-group">
+          <v-btn
+            class="font-montserrat text-capitalize font-weight-bold font-large bg-primary"
+            @click="enviar"
+          >
+            Enviar
+          </v-btn>
+          <v-btn
+            class="font-montserrat text-capitalize font-weight-bold font-large"
+            @click="cancelar"
+            color="grey"
+          >
+            Cancelar
+          </v-btn>
+        </div>
+      </v-card>
     </div>
   </template>
   
-  
   <script setup>
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
   
+  const email = ref('');
+  const router = useRouter();
+  
+  const enviar = () => {
+    router.push('/(login)/login');
+  };
+  
+  const cancelar = () => {
+    router.push('/(login)/login');
+  };
   </script>
+  
+  <style scoped>
+  .recover-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+  
+  .recover-card {
+    width: 50%;
+    text-align: center;
+  }
+  
+  .description {
+    margin-bottom: 15px;
+    font-size: 14px;
+    color: #666;
+  }
+  
+  .button-group {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    gap: 10px;
+    margin-top: 10px;
+  }
+  </style>
   
--- blueberry-Web/src/pages/(login)/terminos.vue
+++ blueberry-Web/src/pages/(login)/terminos.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="header-container">
-      <h1 class="page-title">Términos y Condiciones</h1>
-    </div>
-  </template>
+    <router-link :to="'/(login)/login'">
+      <ExitButton />
+    </router-link>
+    <h1 class="page-title">Términos y Condiciones</h1>
+  </div>
+</template>
   
   
   <script setup>
-  
+    import ExitButton from '../../components/ExitButton.vue';
   </script>
   
