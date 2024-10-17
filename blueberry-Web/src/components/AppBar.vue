137dbeea1d364f9cddb28c9146685efa5508d7a4
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-10-17 12:34:39 -0300

    Add clickable title to home

--- blueberry-Web/src/components/AppBar.vue
+++ blueberry-Web/src/components/AppBar.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app-bar :elevation="2" class="bg-primary">
     <template v-slot:prepend>
-      <img title="Blueberry icon" class="size-12 ml-4 -mr-3" src="../assets/images/blueberry-shadow.png"/>
+      <router-link :to="'/(main)/inicio'" class="d-flex align-center gap-2">
+        <img title="Blueberry icon" class="size-12 ml-4 -mr-3" src="../assets/images/blueberry-shadow.png"/>
+        <v-app-bar-title class="app-title ml-2">Blueberry</v-app-bar-title>
+      </router-link>
     </template>
-    <v-app-bar-title class="app-title">Blueberry</v-app-bar-title>
     <v-spacer></v-spacer>
-    <v-btn prepend-icon="mdi-account" variant="tonal" :to="'/(main)/perfil'" class="font-varela text-capitalize font-bold mr-5 " v-if="isMainRoute">
+    <v-btn prepend-icon="mdi-account" variant="tonal" :to="'/(main)/perfil'" class="font-varela text-capitalize font-bold mr-5" v-if="isMainRoute">
       Manuel Ahumada
     </v-btn>
   </v-app-bar>
