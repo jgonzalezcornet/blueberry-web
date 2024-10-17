17553b5f564b7a595ce16252743b79eea5b954ff
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-10-17 14:11:45 -0300

    Add tarjetas view with card component

---
+++ blueberry-Web/src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <router-link :to="`/(main)/tarjetas/${cardID}`" class="w-100 d-flex">
+    <v-row :style="{ backgroundColor: background, color: '#FFFFFF' }" class="d-flex flex-column align-center ga-4 w-100 rounded-lg overflow-hidden px-3 pb-1 pt-3 text--white">
+      <div class="d-flex w-100 justify-end">
+        <img :src="`/img/${brand}.png`" :style="{ height: '25px' }" />
+      </div>
+      <div class="d-flex align-center justify-between w-100">
+        <p class="text-h5">•••• {{ number }}</p>
+        <img src="/img/arrowRight.png" />
+      </div>
+    </v-row>
+  </router-link>
+</template>
+
+<script setup>
+  defineProps({
+    number: Number,
+    cardID: String,
+    background: {
+      type: String,
+      default: "black"
+    },
+    brand: String
+  });
+</script>
--- blueberry-Web/src/pages/(main)/tarjetas.vue
+++ blueberry-Web/src/pages/(main)/tarjetas.vue
@@ -5,9 +5,30 @@
     </router-link>
     <h1 class="page-title">Tarjetas</h1>
   </div>
+  <div class="page-content">
+    <v-card elevation="2" class="font-montserrat px-6 pt-6 pb-7 rounded-lg w-66 d-flex flex-column align-center ga-8">
+      <Card number="7778" cardID="1" background="#16193C" brand="visa" />
+      <Card number="1595" cardID="2" background="#AF9B30" brand="visa" />
+      <Card number="8481" cardID="3" brand="mastercard" />
+    </v-card>
+    <v-btn class="text-capitalize font-montserrat font-weight-bold font-large bg-primary mt-5" color="primary">Agregar tarjeta</v-btn>
+  </div>
 </template>
   
   
 <script setup>
   import ExitButton from '../../components/ExitButton.vue';
+  import Card from '../../components/Card.vue';
 </script>
+
+<style scoped>
+  .card {
+    width: 100%;
+    min-height: 80px;
+    padding: 8px;
+    background-color: #CCCCCC;
+    display: flex;
+    flex-direction: column;
+    border-radius: 8px;
+  }
+</style>
