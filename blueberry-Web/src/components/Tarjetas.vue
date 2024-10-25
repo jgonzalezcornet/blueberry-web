d410d46a9bac8c66714248338e7b7e7efad13ae7
maahumada <maahumada@itba.edu.ar>
2024-10-25 12:24:01 -0300

    Remove card container if no cards available

--- blueberry-Web/src/components/Tarjetas.vue
+++ blueberry-Web/src/components/Tarjetas.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card elevation="2" class="font-montserrat px-6 pt-6 pb-7 rounded-lg w-50 d-flex flex-column align-center ga-8">
+  <v-card v-if="tarjetas.length" elevation="2" class="font-montserrat px-6 pt-6 pb-7 rounded-lg w-50 d-flex flex-column align-center ga-8">
     <Card v-for="tarjeta in tarjetas" :number="tarjeta.number % 10000" :cardID="tarjeta.id" :brand="getCardBrand(tarjeta.number)" />
   </v-card>
 </template>
